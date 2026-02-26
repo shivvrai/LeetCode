5d053ecf89ece70b9713dd9503855c6c8b918346
Yashmit Rai <shivrai0107@gmail.com>
2026-02-26 13:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (80.57%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,  nums1);
         }
+        int m=nums1.size();
+        int n=nums2.size();
+        int l=0,h=m;
+        while(l<=h){
+            int mid1=(l+h)/2;
+            int mid2=(n+m+1)/2-mid1;
+            int l1=(mid1==0)?INT_MIN:nums1[mid1-1];
+            int l2=(mid2==0)?INT_MIN:nums2[mid2-1];
+            int r1=(mid1==m)?INT_MAX:nums1[mid1];
+            int r2=(mid2==n)?INT_MAX:nums2[mid2];
+            if(l1<=r2 && l2<=r1){
+                if((m+n)%2==0)return (double)(min(r1,r2)+max(l1,l2))/2;
+                else return max(l1,l2);
 
-        int len1 = nums1.size(), len2 = nums2.size();
-        int left = 0, right = len1;
-
-        while (left <= right) {
-            int part1 = (left + right) / 2;
-            int part2 = (len1 + len2 + 1) / 2 - part1;
-
-            int maxLeft1 = (part1 == 0) ? INT_MIN : nums1[part1 - 1];
-            int minRight1 = (part1 == len1) ? INT_MAX : nums1[part1];
-            int maxLeft2 = (part2 == 0) ? INT_MIN : nums2[part2 - 1];
-            int minRight2 = (part2 == len2) ? INT_MAX : nums2[part2];
-
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0;
-                } else {
-                    return max(maxLeft1, maxLeft2);
-                }
-            } else if (maxLeft1 > minRight2) {
-                right = part1 - 1;
-            } else {
-                left = part1 + 1;
             }
+            else if (l1>r2) h=mid1-1;
+            else l=mid1+1;
         }
+        return 0.0;
 
-        return 0.0;        
     }
 };
