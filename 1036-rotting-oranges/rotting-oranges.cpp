4b9611dafc1f33d212c548f626e368e0fa433f2f
Yashmit Rai <shivrai0107@gmail.com>
2026-04-02 01:34:46 +0530

    Time: 3 ms (27.54%) | Memory: 16.8 MB (70.67%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,59 +1,40 @@
 class Solution {
 public:
-    // int func(){
-    //     if()
-    // }
-
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        
-        queue<pair<int,pair<int,int>>> q;
-        vector<vector<int>> visited(n, vector<int>(m, -1));
-
-        // push all rotten oranges
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 2) {
-                    q.push({0, {i, j}});
-                    visited[i][j] = 1;
+        int n=grid.size();
+        int m=grid[0].size();int freshoranges=0,time=0;
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
                 }
+                else if(grid[i][j]==1)freshoranges++;
             }
         }
-
-        int maxi = 0;
-        vector<int> dr = {0, 1, -1, 0};
-        vector<int> dc = {-1, 0, 0, 1};
-
-        while(!q.empty()) {
-            auto t = q.front();
-            q.pop();
-
-            int time = t.first;
-            int i = t.second.first;
-            int j = t.second.second;
-
-            maxi = max(maxi, time);
-
-            for(int k = 0; k < 4; k++) {
-                int ni = i + dr[k];
-                int nj = j + dc[k];
-
-                if(ni >= 0 && nj >= 0 && ni < n && nj < m &&
-                   grid[ni][nj] == 1 && visited[ni][nj] == -1) {
-
-                    visited[ni][nj] = 1;
-                    q.push({time + 1, {ni, nj}});
+        vector<int> dx={1,-1,0,0};
+        vector<int> dy={0,0,-1,1};
+        if(freshoranges==0)return time;
+        while(!q.empty()){
+            int level=q.size();
+            bool rotten=false;
+            for(int i=0;i<level;i++){
+                auto p=q.front();
+                int x=p.first;
+                int y=p.second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int nx=x+dx[i];
+                    int ny=y+dy[i];
+                    if(nx>=0 and ny>=0 and nx<n and ny<m and grid[nx][ny]==1){
+                        q.push({nx,ny});
+                        grid[nx][ny]=2;
+                        freshoranges--;rotten=true;
+                    }
                 }
             }
+            if(rotten)time++;
         }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1 && visited[i][j] == -1) {
-                    return -1;
-                }
-            }
-        }
-
-        return maxi;
+        return (freshoranges==0 ? time : -1);
     }
 };
