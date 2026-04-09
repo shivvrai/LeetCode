64090858e46295e41c81b5b9b7029cd4868b7acf
Yashmit Rai <shivrai0107@gmail.com>
2026-04-09 23:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (56.59%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>sum)return false;
+            sum=max(sum,i+nums[i]);
+        }
+        return true;
+    }
+};
