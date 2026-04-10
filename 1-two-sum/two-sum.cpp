fccf5a0d3afaf0e8c1725b89645444b724576dd7
Yashmit Rai <shivrai0107@gmail.com>
2026-04-10 15:01:43 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (53.23%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -6,7 +6,7 @@ public:
 
         for (int i = 0; i < n; i++) {
             int complement = target - nums[i];
-            if (numMap.count(complement)) {
+            if (numMap.find(complement)!=numMap.end()) {
                 return {numMap[complement], i};
             }
             numMap[nums[i]] = i;
