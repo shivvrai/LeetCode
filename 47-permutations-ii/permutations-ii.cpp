1941a090e170f9f2d2de57b7abe59d36335a621b
Yashmit Rai <shivrai0107@gmail.com>
2026-04-12 23:39:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (76.56%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,37 +1,27 @@
 class Solution {
-public:
-    vector<vector<int>> result;
-
-    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& temp) {
-        if (temp.size() == nums.size()) {
-            result.push_back(temp);
-            return;
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            // Skip used elements
-            if (used[i]) continue;
-
-            // Skip duplicates
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
-                continue;
-
-            used[i] = true;
-            temp.push_back(nums[i]);
-
-            backtrack(nums, used, temp);
-
-            temp.pop_back();
-            used[i] = false;
-        }
+public:int n=0;
+    void func(vector<vector<int>>&ans,vector<int>&curr,vector<int>&arr,vector<bool>&used){
+    if(curr.size()==n){
+        ans.push_back(curr);
+        return;
     }
+    for(int i=0;i<n;i++){
+        if(used[i])continue;
+        if(i>0 and arr[i]==arr[i-1] and !used[i-1])continue;
 
+        used[i]=true;
+        curr.push_back(arr[i]);
+        func(ans,curr,arr,used);
+        curr.pop_back();
+        used[i]=false;
+    }
+}
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<bool> used(nums.size(), false);
-        vector<int> temp;
-
-        backtrack(nums, used, temp);
-        return result;
+        n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int>curr;vector<vector<int>>ans;
+        vector<bool>used(n,false);
+        func(ans,curr,nums,used);
+        return ans;
     }
-}; 
+};
