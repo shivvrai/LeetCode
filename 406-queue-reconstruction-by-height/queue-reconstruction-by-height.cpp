102d75bf3f43bfc15f055585e653c264a84d656a
Yashmit Rai <shivrai0107@gmail.com>
2026-04-12 18:22:22 +0530

    Time: 24 ms (21.12%) | Memory: 16 MB (80.72%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    static bool comp(vector<int>& a, vector<int>& b){ //Comparator function for the logic of sort
-        if(a[0] == b[0]) return a[1] < b[1];
-        return a[0] > b[0];
-    }
-    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {   
-        sort(people.begin(), people.end(), comp);  //Sorting the array according to comp function.
-        vector<vector<int>> ans;                   //to store the final array to be returned
-        for(auto man : people){
-            ans.insert(ans.begin()+man[1], man);   //Inserting the element at desired position.
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), [](vector<int>& a, vector<int>& b){
+            return (a[0] == b[0])? a[1] < b[1] : a[0] > b[0];
+        });
+
+        vector<vector<int>> result;
+        
+        for (auto& person : people) {
+            result.insert(result.begin() + person[1], person);
         }
-        return ans;
+
+        return result;
     }
 };
