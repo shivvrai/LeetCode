0547d5b607f112bd9a31d80d837bf4e96b6b0fad
Yashmit Rai <shivrai0107@gmail.com>
2026-04-20 17:39:09 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (58.02%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -10,15 +10,15 @@
  * };
  */
 class Solution {
-public:
-int sum=0;
-void func(TreeNode* root,int isleft){
-    if(!root)return;
-    if(!root->left and !root->right and isleft)sum+=root->val;
-    if(root->left)func(root->left,1);
-    if(root->right)func(root->right,0);
-    
-}
+public:int sum=0;
+    void func(TreeNode* root,int l){
+        if(!root)return;
+        if(l==1 and !root->left and !root->right){
+            sum+=root->val;
+        }
+        if(root->left)func(root->left,1);
+        if(root->right)func(root->right,0);
+    }
     int sumOfLeftLeaves(TreeNode* root) {
         func(root,0);
         return sum;
