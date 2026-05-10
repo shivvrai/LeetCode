568fe03c5c31c1af29cef77e7889f6a4ccdd958b
Yashmit Rai <shivrai0107@gmail.com>
2026-05-10 11:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (60.11%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -16,8 +16,8 @@ public:
         TreeNode* temp=root->left;
         root->left=root->right;
         root->right=temp;
-        invertTree(root->right);
         invertTree(root->left);
+        invertTree(root->right);
         return root;
     }
 };
