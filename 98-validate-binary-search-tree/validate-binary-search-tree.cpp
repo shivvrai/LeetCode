1b2b5de1814805da6da3bf06af032abfcb8502a7
Yashmit Rai <shivrai0107@gmail.com>
2026-05-26 09:10:08 +0530

    Time: 11 ms (1.62%) | Memory: 31.9 MB (7.70%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,30 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& ans) {
-        if (root == NULL)
-            return;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
+    bool func(TreeNode* root,vector<long long>cond){
+        if(!root)return true;
+        if(root->val<=cond[0] || root->val>=cond[1])return false;
+        bool left=func(root->left,{cond[0],root->val});
+        bool right=func(root->right,{root->val,cond[1]});
+        return left and right;
     }
-
     bool isValidBST(TreeNode* root) {
-        vector<int> in;
-        inorder(root, in);
+        vector<long long >cond={LLONG_MIN,LLONG_MAX};
+        return func(root,cond);
+        
 
-        for (int i = 1; i < in.size(); i++) {
-            if (in[i - 1] < in[i])
-                continue;
-            else
-                return false;
-        }
-        return true;
     }
 };
