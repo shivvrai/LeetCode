a292b1c151df5ab37f7a8a1d3bd4b5c41559e66b
Yashmit Rai <shivrai0107@gmail.com>
2026-06-05 05:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -7,10 +7,16 @@ public:
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,0);
-
-        dp[0]=1;dp[1]=1;
+        if(n <= 1) return 1;
+        int prev1=1;int prev2=1;int curr=0;
         for(int i=2;i<=n;i++){
-         dp[i]=dp[i-1]+dp[i-2];}
-        return dp[n];
+        
+        
+        curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+         }
+
+        return curr;
     }
 };
