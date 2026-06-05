577d0590f5af96faec8e50f4992d6ec68ec755cd
Yashmit Rai <shivrai0107@gmail.com>
2026-06-05 06:31:57 +0530

    Time: 99 ms (22.13%) | Memory: 82.1 MB (21.80%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     class compare{
         public:
-        bool operator()(vector<int>&a ,vector<int>&b){
-            return (a[0]*a[0] +a[1]*a[1])<(b[0]*b[0]+b[1]*b[1]);
+        bool operator()(vector<int>&a,vector<int>&b){
+            return ((a[0]*a[0]+a[1]*a[1])<(b[0]*b[0]+b[1]*b[1]));
         }
     };
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         priority_queue<vector<int>,vector<vector<int>>,compare>pq;
         for(auto i:points){
-            
             pq.push(i);
             if(pq.size()>k)pq.pop();
         }
         vector<vector<int>>ans;
-        while(pq.size()){
-            ans.push_back(pq.top());pq.pop();
+        for(int i=0;i<k;i++){
+            ans.push_back(pq.top());
+            pq.pop();
         }
         return ans;
-
+        
     }
 };
