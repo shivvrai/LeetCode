e0127a61f7ef28d7f87248b3ce9ec812594506fc
Yashmit Rai <shivrai0107@gmail.com>
2026-06-05 08:06:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (61.40%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int curr = max(prev1, nums[i] + prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1)
+            return nums[0];
+
+        return max(
+            solve(nums, 0, n - 2), // exclude last
+            solve(nums, 1, n - 1)  // exclude first
+        );
+    }
+};
