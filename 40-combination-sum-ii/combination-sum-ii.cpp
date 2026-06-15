70cf7d700188ceb0efb0642e1ca5750dffba7491
Yashmit Rai <shivrai0107@gmail.com>
2026-06-15 06:57:43 +0530

    Time: 2 ms (43.89%) | Memory: 14 MB (82.94%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,29 +1,24 @@
 class Solution {
-public:int k=0;
-    void func(int index,vector<vector<int>>&ans,
-        vector<int>&curr,vector<int>&arr,int sum){
-        if(sum==k){
+public:
+    void func(int i,vector<int>&curr,vector<int>&arr,vector<vector<int>>&ans,int sum){
+        if(sum==0){
             ans.push_back(curr);
             return;
         }
-        for(int i=index;i<arr.size();i++){
-            if(i>index and arr[i]==arr[i-1])continue;
-            if(sum+arr[i]<=k){
-                sum+=arr[i];
-                curr.push_back(arr[i]);
-                func(i+1,ans,curr,arr,sum);
-                curr.pop_back();
-                sum-=arr[i];
-            }
-
+        if(i>=arr.size() or sum<0)return;
+        for(int j=i;j<arr.size();j++){
+            if(j>i and arr[j]==arr[j-1])continue;
+            curr.push_back(arr[j]);
+            func(j+1,curr,arr,ans,sum-arr[j]);
+            curr.pop_back();
         }
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;k=target;
         sort(candidates.begin(),candidates.end());
-        vector<int>curr;
-        func(0,ans,curr,candidates,0);
+         vector<int>curr;
+        
+        vector<vector<int>>ans;
+        func(0,curr,candidates,ans,target);
         return ans;
     }
 };
