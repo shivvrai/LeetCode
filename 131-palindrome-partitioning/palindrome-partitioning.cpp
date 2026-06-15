ab1ce54f4d7d35d6524c477020a8960699fae7ad
Yashmit Rai <shivrai0107@gmail.com>
2026-06-16 01:04:24 +0530

    Time: 131 ms (9.79%) | Memory: 144.8 MB (8.87%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> pars;
-        vector<string> par;
-        partition(s, 0, par, pars);
-        return pars;
-    }
-private: 
-    void partition(string& s, int start, vector<string>& par, vector<vector<string>>& pars) {
-        int n = s.length();
-        if(start==n){
-            pars.push_back(par);
-        }else{
-            for(int i = start ; i < n ;i ++){
-               if(isPalindrome(s,start,i)){
-                   par.push_back(s.substr(start,i-start+1));
-                   partition(s,i+1,par,pars);
-                   par.pop_back();
-               }
-            }
-        }
-    }
-    
-    bool isPalindrome(string& s, int l, int r) {
-        while (l < r) {
-            if (s[l++] != s[r--]) {
-                return false;
-            }
+    bool ispalindrome(string s,int l,int r){
+        while(l<r){
+            if(s[l++]!=s[r--])return false;
         }
         return true;
+    }
+    void func(int start,string& s,vector<string> curr,vector<vector<string>>& ans){
+        if(start==s.length()){
+            ans.push_back(curr);
+            return;
+        }
+        for(int end=start+1;end<=s.size();end++){
+            if(ispalindrome(s,start,end-1)){
+                curr.push_back(s.substr(start,end-start));
+                func(end,s,curr,ans);
+                curr.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> curr;
+        func(0,s,curr,ans);
+        return ans;
     }
 };
