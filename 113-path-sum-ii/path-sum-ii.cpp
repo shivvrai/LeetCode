52f47fde835d4d7e7edd714b9d9d2c2dfe4d52a6
Yashmit Rai <shivrai0107@gmail.com>
2026-06-16 01:38:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (88.19%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -11,23 +11,23 @@
  */
 class Solution {
 public:
-    void func(TreeNode* root, int k,int sum,vector<vector<int>>&ans,vector<int>&curr){
-        if(!root){
-            return;
-        }
-        curr.push_back(root->val);sum+=root->val;
-        if(!root->left and !root->right){
-            if(k==sum)
-            ans.push_back(curr);
-            
-        }
-        func(root->left,k,sum,ans,curr);
-        func(root->right,k,sum,ans,curr);
-        curr.pop_back();
+void func(TreeNode* root,int sum,vector<int>&path,vector<vector<int>>&ans){
+    if(!root)return;
+    if(!root->left and !root->right and sum-root->val==0){
+        path.push_back(root->val);
+        ans.push_back(path);
+        path.pop_back();
+        return;
     }
+    path.push_back(root->val);
+    func(root->left,sum-root->val,path,ans);
+    func(root->right,sum-root->val,path,ans);
+    path.pop_back();
+}
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>>ans;vector<int>curr;
-        func(root,targetSum,0,ans,curr);
+        vector<int>path;
+        vector<vector<int>>ans;
+        func( root, targetSum,path,ans);
         return ans;
     }
 };
