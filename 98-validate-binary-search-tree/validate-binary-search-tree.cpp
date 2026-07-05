364bcd0da8761d2ffd9c527829a343d37bf21725
Yashmit Rai <shivrai0107@gmail.com>
2026-07-05 13:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (44.47%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -10,18 +10,20 @@
  * };
  */
 class Solution {
-public:
-    bool func(TreeNode* root,vector<long long>cond){
+public: 
+    TreeNode * prev=nullptr;
+    bool func(TreeNode* root){
         if(!root)return true;
-        if(root->val<=cond[0] || root->val>=cond[1])return false;
-        bool left=func(root->left,{cond[0],root->val});
-        bool right=func(root->right,{root->val,cond[1]});
+        bool left= func(root->left);
+        if(prev and prev->val>=root->val)return false;
+        prev=root;
+        
+        bool right= func(root->right);
+        
         return left and right;
+
     }
     bool isValidBST(TreeNode* root) {
-        vector<long long >cond={LLONG_MIN,LLONG_MAX};
-        return func(root,cond);
-        
-
+        return func(root);
     }
 };
