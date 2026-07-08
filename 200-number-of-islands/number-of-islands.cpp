50c5709c1606ee31b579e32d019a7fb9c62a4969
Yashmit Rai <shivrai0107@gmail.com>
2026-07-08 14:23:22 +0530

    Time: 25 ms (72.50%) | Memory: 16.3 MB (80.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,13 +3,14 @@ public:
     void dfs(vector<vector<char>>& grid,int i,int j){
         if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or grid[i][j]=='0')return;
         grid[i][j]='0';
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
         dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
         dfs(grid,i+1,j);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size(),m=grid[0].size();int cnt=0;
+        int n=grid.size();int cnt=0;
+        int m=grid[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
@@ -19,5 +20,6 @@ public:
             }
         }
         return cnt;
+        
     }
 };
