65a73e1e7e4baf265aad2336e15569847d291e9c
Yashmit Rai <shivrai0107@gmail.com>
2026-07-14 03:29:47 +0530

    Time: 3 ms (58.99%) | Memory: 16.2 MB (50.29%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -3,26 +3,27 @@ public:
     ListNode* removeZeroSumSublists(ListNode* head) {
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
-        int prefix_sum = 0;
-        std::unordered_map<int, ListNode*> prefix_sums;
-        prefix_sums[0] = dummy;
-        ListNode* current = head;
 
-        while (current) {
-            prefix_sum += current->val;
-            if (prefix_sums.find(prefix_sum) != prefix_sums.end()) {
-                ListNode* to_delete = prefix_sums[prefix_sum]->next;
-                int temp_sum = prefix_sum + to_delete->val;
-                while (to_delete != current) {
-                    prefix_sums.erase(temp_sum);
-                    to_delete = to_delete->next;
-                    temp_sum += to_delete->val;
-                }
-                prefix_sums[prefix_sum]->next = current->next;
-            } else {
-                prefix_sums[prefix_sum] = current;
-            }
-            current = current->next;
+        unordered_map<int, ListNode*> mp;
+
+        int prefix = 0;
+        ListNode* curr = dummy;
+
+        // First pass: store last occurrence of every prefix sum
+        while (curr) {
+            prefix += curr->val;
+            mp[prefix] = curr;
+            curr = curr->next;
+        }
+
+        // Second pass: remove zero-sum sublists
+        prefix = 0;
+        curr = dummy;
+
+        while (curr) {
+            prefix += curr->val;
+            curr->next = mp[prefix]->next;
+            curr = curr->next;
         }
 
         return dummy->next;
