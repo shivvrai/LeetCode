9e5cebe494401b7bee9fd83fe5095de92530a4bd
Yashmit Rai <shivrai0107@gmail.com>
2026-07-21 15:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (64.41%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -10,7 +10,16 @@ public:
     }
     int rob(vector<int>& nums) {
         vector<int>dp(nums.size(),-1);
-        return func(nums,nums.size()-1,dp);
+        // return func(nums,nums.size()-1,dp);
+        if(nums.size()==1)return nums[0];
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            int pick=nums[i]+dp[i-2];
+            int notpick=dp[i-1];
+            dp[i]=max(pick,notpick);
+        }
+        return dp[nums.size()-1];
         
     }
 };
