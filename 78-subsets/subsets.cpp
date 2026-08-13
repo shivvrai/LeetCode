b1df26ef03fdc08be023e72db7fdbe6109a74f60
Yashmit Rai <shivrai0107@gmail.com>
2026-08-13 10:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (96.61%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    void func(int start,vector<int>&curr,vector<int>&arr,vector<vector<int>>&ans){
+     void func(int j,vector<int>&curr,vector<int>&arr,vector<vector<int>>&ans){
         ans.push_back(curr);
-        for(int i=start;i<arr.size();i++){
+        for(int i=j;i<arr.size();i++){
             curr.push_back(arr[i]);
             func(i+1,curr,arr,ans);
             curr.pop_back();
         }
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>curr;
+     }
+    vector<vector<int>> subsets(vector<int>& arr) {
         vector<vector<int>>ans;
-        func(0,curr,nums,ans);
+        vector<int>curr;
+        func(0,curr,arr,ans);
         return ans;
+        
     }
 };
