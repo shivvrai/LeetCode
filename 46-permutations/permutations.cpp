3779e35b78fb6af5f85307508b14821a78328ea2
Yashmit Rai <shivrai0107@gmail.com>
2026-08-13 10:07:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (89.04%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void func(vector<int>&curr,vector<int>&arr,vector<vector<int>>&ans,vector<bool>&used){
-        if(curr.size()==arr.size()){
-            ans.push_back(curr);return;
-        }
+    void func(vector<int>&arr, vector<int>&curr,vector<vector<int>>& ans, vector<bool>&used){
+        if(curr.size()==arr.size()){ans.push_back(curr);return;}
+
         for(int i=0;i<arr.size();i++){
             if(used[i])continue;
-            curr.push_back(arr[i]);
             used[i]=true;
-            func(curr,arr,ans,used);
-            used[i]=false;
+            curr.push_back(arr[i]);
+            func(arr,curr,ans,used);
             curr.pop_back();
+            used[i]=false;
         }
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>curr;
-        vector<bool>used(n,false);
+        vector<bool>used(nums.size(),false);
         vector<vector<int>>ans;
-        func(curr,nums,ans,used);
+        vector<int>curr;
+        func(nums,curr,ans,used);
         return ans;
+        
     }
 };
